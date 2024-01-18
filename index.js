3d0dccd5739806248241766e0507ab83ff39251e
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-01-18 06:32:12 +0100

    Add basic knightMoves function

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ let visitedChessFields = [
   [0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-const knightPosition = { Y: 3, X: 3 };
+const knightPosition = { x: 3, y: 3 };
 
-console.log(knightPosition.Y);
+console.log(knightPosition.x);
+
+// Write a function that returns an array with all the possible moves from the knights current position
+function knightMoves(x, y) {
+  const array = [];
+  array.push([x + 2, y + 1]);
+  array.push([x + 2, y - 1]);
+  array.push([x - 2, y + 1]);
+  array.push([x - 2, y - 1]);
+  array.push([x + 1, y + 2]);
+  array.push([x + 1, y - 2]);
+  array.push([x - 1, y + 2]);
+  array.push([x - 1, y - 2]);
+  return array;
+}
+
+console.log(knightMoves(3, 3));
