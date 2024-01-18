20dfe1c45d8008c20691de2eab2c62bda8e41d09
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-01-18 21:57:48 +0100

    Improve knightMoves function

--- index.js
+++ index.js
@@ -15,16 +15,26 @@ console.log(knightPosition.x);
 
 // Write a function that returns an array with all the possible moves from the knights current position
 function knightMoves(x, y) {
-  const array = [];
-  array.push([x + 2, y + 1]);
-  array.push([x + 2, y - 1]);
-  array.push([x - 2, y + 1]);
-  array.push([x - 2, y - 1]);
-  array.push([x + 1, y + 2]);
-  array.push([x + 1, y - 2]);
-  array.push([x - 1, y + 2]);
-  array.push([x - 1, y - 2]);
-  return array;
+  const moves = [];
+  moves.push([x + 2, y + 1]);
+  moves.push([x + 2, y - 1]);
+  moves.push([x - 2, y + 1]);
+  moves.push([x - 2, y - 1]);
+  moves.push([x + 1, y + 2]);
+  moves.push([x + 1, y - 2]);
+  moves.push([x - 1, y + 2]);
+  moves.push([x - 1, y - 2]);
+
+  // Remove moves against the rules
+  moves.forEach((move, index) => {
+    move.forEach((num) => {
+      if (num < 0) {
+        console.log(`Move ${index} is invalid!`);
+      }
+    });
+  });
+
+  return moves;
 }
 
 console.log(knightMoves(3, 3));
