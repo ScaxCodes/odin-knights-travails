ec0366141c8cb82ad4f282f5e7a2f0cedcfe3a0d
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-01-19 05:46:22 +0100

    Add functionality to remove invalid moves from array

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let visitedChessFields = [
 
 const knightPosition = { x: 3, y: 3 };
 
-console.log(knightPosition.x);
+console.log(`Knights position: ${knightPosition.x}, ${knightPosition.y}`);
 
 // Write a function that returns an array with all the possible moves from the knights current position
 function knightMoves(x, y) {
@@ -28,8 +28,9 @@ function knightMoves(x, y) {
   // Remove moves against the rules
   moves.forEach((move, index) => {
     move.forEach((num) => {
-      if (num < 0) {
-        console.log(`Move ${index} is invalid!`);
+      if (num < 0 || num > 7) {
+        console.log(`Move ${index + 1} is invalid!`);
+        moves.splice(index, 1);
       }
     });
   });
@@ -37,4 +38,9 @@ function knightMoves(x, y) {
   return moves;
 }
 
-console.log(knightMoves(3, 3));
+console.log(knightMoves(knightPosition.x, knightPosition.y));
+
+// Write a function to change position, test other cases during same run
+
+// TODO:
+// Works with 3,3 and 3,1, but not with 0,0... why?
