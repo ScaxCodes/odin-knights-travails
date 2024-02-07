4ff1bb331906163b238d8719797f1ff47a99d1ee
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-02-07 06:38:45 +0100

    Provide a first fully functional solution

--- index.js
+++ index.js
@@ -1,42 +1,82 @@
-// TODO: Nochmal neu konzeptionieren von vorne mit Pseudecode und data structures
-// dann Lösung anschauen, dann nochmal nachbauen, alternativ chatgpt fragen a la CC
-
-// Not needed for the current solution
-// let visitedChessFields = [
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-// ];
-
 class Move {
   constructor(x, y, prev = null) {
-    this.position = { x, y };
-    this.prev = prev;
-    // .next could be an object with 8 positions and the bool prop validMove
-    this.next = [];
+    this.x = x; // x-coordinate of the field
+    this.y = y; // y-coordinate of the field
+    this.prev = prev; // Reference to the previous Move object
   }
 }
 
-const tree = { 1: [] };
+class Queue {
+  constructor() {
+    this.items = [];
+  }
 
-// const test = new Move();
-// const test2 = new Move(2, 3);
-// const test3 = new Move(5, 5, test2);
+  push(element) {
+    this.items.push(element);
+  }
 
-// console.log(test, test2, test3);
-// console.log(test3.prev.position.x);
+  pop() {
+    if (this.isEmpty()) {
+      return "Error - queue is empty!";
+    }
+    return this.items.shift();
+  }
 
-const knightPosition = { x: 3, y: 3 };
-const knightTarget = { x: 7, y: 3 };
+  isEmpty() {
+    return this.items.length === 0;
+  }
+}
 
-console.log(`Knights position: ${knightPosition.x}, ${knightPosition.y}`);
+function shortestPath(start, target) {
+  const queue = new Queue();
+  const visited = new Set();
 
-// Write a function that returns an array with all the possible moves from the knights current position
+  // Add start field to the queue
+  queue.push(start);
+  visited.add(`${start.x},${start.y}`);
+
+  while (!queue.isEmpty()) {
+    const currentMove = queue.pop();
+
+    // Check if the current field is the target field
+    if (currentMove.x === target.x && currentMove.y === target.y) {
+      // We have found the shortest path, so we backtrack to create the path
+      return getPath(currentMove);
+    }
+
+    // Add all possible next moves that the knight can make
+    const possibleMoves = knightPossibleMoves(currentMove.x, currentMove.y);
+    possibleMoves.forEach((move) => {
+      const [nextX, nextY] = move;
+      // Check if the field is within the chessboard and if it has not been visited already
+      if (
+        nextX >= 0 &&
+        nextX < 8 &&
+        nextY >= 0 &&
+        nextY < 8 &&
+        !visited.has(`${nextX},${nextY}`)
+      ) {
+        const nextMove = new Move(nextX, nextY, currentMove);
+        queue.push(nextMove);
+        visited.add(`${nextX},${nextY}`);
+      }
+    });
+  }
+
+  // If no path is found, return null
+  return null;
+}
+
+function getPath(move) {
+  const path = [];
+  let currentMove = move;
+  while (currentMove !== null) {
+    path.unshift([currentMove.x, currentMove.y]);
+    currentMove = currentMove.prev;
+  }
+  return path;
+}
+
 function knightPossibleMoves(x, y) {
   const moves = [];
   moves.push([x + 2, y + 1]);
@@ -51,85 +91,40 @@ function knightPossibleMoves(x, y) {
   const validMoves = [];
   moves.forEach((move, index) => {
     if (move[0] < 0 || move[1] < 0 || move[0] > 7 || move[0] > 7) {
-      // console.log(`Move ${index + 1} is invalid!`);
     } else {
       validMoves.push(move);
-      // console.log(`Move ${index + 1} is valid!`);
     }
   });
-  // console.log("Valid moves are:");
-  // console.log(validMoves);
   return validMoves;
 }
 
-function calcLevel(x, y) {
-  const currentLevel = Object.entries(tree).length;
-  console.log(currentLevel);
-  if (currentLevel == 1) {
-    const possibleMoves = knightPossibleMoves(x, y);
-    console.log(possibleMoves);
-    const root = new Move(x, y);
-    possibleMoves.forEach((move) => {
-      root.next.push(move);
-    });
-    console.log(root);
-    tree[1].push(root);
-  } else {
-  }
-}
-
-calcLevel(3, 3);
-searchTarget();
-
-function searchTarget() {
-  const currentLevel = parseInt(Object.entries(tree).length);
-  for (field of tree[currentLevel][0].next) {
-    if (field[0] == knightTarget.x && field[1] == knightTarget.y) {
-      console.log(`Target reached in ${currentLevel} turn!`);
-      return;
-    }
-  }
-  console.log(`${currentLevel} turn was not enough, calculating next turn...`);
-  tree[currentLevel + 1] = [];
-}
-
-// TODO: Write a func that checks if the array contains the target field
-// if yes, print result
-// if no, repeat knightPossiblemoves with all other fields from the array
-// link the moves via tree with up to 8 childs
-// remember to do many little functions instead of one big (single responsibility principle)
-// TODO: Think about the data structure and search algorythm before going further
-
-// function knightMoves(knightPosition, knightTarget) {
-//   const possibleMoves = knightPossibleMoves(
-//     knightPosition[0],
-//     knightPosition[1]
-//   );
-//   for (field of possibleMoves) {
-//     if (field[0] == knightTarget[0] && field[1] == knightTarget[1]) {
-//       console.log("Target reached in one turn!");
-//       console.log(knightPosition, knightTarget);
-//       return;
-//     }
-//   }
-//   console.log("1 turn was not enough, calculating next turn...");
-// }
-
 // TESTING
-// knightMoves([3, 3], [5, 4]);
+console.log("Testing knightPossibleMoves:");
+console.log(knightPossibleMoves(3, 3)); // Expected: [[5, 4], [5, 2], [1, 4], [1, 2], [4, 5], [4, 1], [2, 5], [2, 1]]
 
-// function setKnightPosition(x, y) {
-//   knightPosition.x = x;
-//   knightPosition.y = y;
-// }
+const start = new Move(3, 3);
+const target = { x: 7, y: 3 };
 
-// console.log("Testing moves for position 3-3");
-// console.log(knightPossibleMoves(knightPosition.x, knightPosition.y));
+console.log("Testing shortestPath:");
+console.log("Start field:", start);
+console.log("Target field:", target);
+console.log("Shortest path:", shortestPath(start, target)); // Expected: Shortest path as an array of field coordinates
 
-// setKnightPosition(3, 1);
-// console.log("Testing moves for position 3-1");
-// console.log(knightPossibleMoves(knightPosition.x, knightPosition.y));
+start.x = 0;
+start.y = 0;
+target.x = 7;
+target.y = 7;
 
-// setKnightPosition(0, 0);
-// console.log("Testing moves for position 0-0");
-// console.log(knightPossibleMoves(knightPosition.x, knightPosition.y));
+console.log("Testing shortestPath:");
+console.log("Start field:", start);
+console.log("Target field:", target);
+const path = shortestPath(start, target);
+
+if (path) {
+  console.log(`You made it in ${path.length - 1} moves! Here's your path:`);
+  path.forEach((coords) => {
+    console.log(`[${coords}]`);
+  });
+} else {
+  console.log("No path found!");
+}
